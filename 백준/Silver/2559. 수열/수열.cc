42c58f9e462cab7a42ff735d4f3266051361075f
oshyeon <31685487+Sho1007@users.noreply.github.com>
2023-09-05 23:13:54 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,55 +1,35 @@
 #include <iostream>
-
-#define endl "\n"
-
 using namespace std;
 
-int N, K;
+int N, K, Input, Sum = 0, MaxNum;
 
-int* sum;
+int Temp[100'001] = {};
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N >> K;
-
-	sum = new int[N];
-
-	int num;
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> num;
-		if (i == 0)
-			sum[i] = num;
-		else
-			sum[i] = sum[i - 1] + num;
-	}
-
-	int start = K-1;
-	int max = -999'999'999;
-	for (int i = start; i < N; ++i)
-	{
-		int num;
-		if (i == start)
-		{
-			num = sum[i];
-		}
-		else
-		{
-			num = sum[i] - sum[i - K];
-			
-		}
-		if (num > max)
-			max = num;
-
-		
-	}
-	cout << max << endl;
-
-	delete[] sum;
-
-	return 0;
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> N >> K;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> Temp[i];
+        Sum += Temp[i];
+        if (i < K)
+        {
+            if (i == K -1)
+            {
+                MaxNum = Sum;
+            }
+        }
+        else
+        {
+            Sum -= Temp[i-K];
+            MaxNum = MaxNum > Sum ? MaxNum : Sum;
+        }
+    }
+    
+    cout << MaxNum << '\n';
+    
+    return 0;
 }
