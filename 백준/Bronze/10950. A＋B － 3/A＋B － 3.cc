2c1049cff45332865fd1e5d72094de353dca1600
oshyeon <31685487+Sho1007@users.noreply.github.com>
2023-09-07 11:06:15 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, A, B;
+    
+    cin >> N;
+    
+    while (N-->0)
+    {
+        cin >> A >> B;
+        
+        cout << A + B << '\n';
+    }
+    
+    return 0;
+}
