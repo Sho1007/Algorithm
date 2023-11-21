8a152a44fd7a1ff8373e3adde6b539a5753caabe
oshyeon <31685487+Sho1007@users.noreply.github.com>
2023-11-21 13:46:00 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,24 +1,96 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-int N, Input, Target;
+typedef struct Node
+{
+	int Data;
+	Node* NextNode;
 
-map<int,int> Map;
+	Node(int NewData) : Data(NewData), NextNode(nullptr) {}
+} Node;
+
+class SingleLinkedList
+{
+public:
+	~SingleLinkedList()
+	{
+		while (Head != nullptr)
+		{
+			Node* DeleteNode = Head;
+			Head = Head->NextNode;
+			delete DeleteNode;
+		}
+	}
+
+	void Print()
+	{
+		cout << "Data : ";
+		Node* Current = Head;
+		while (Current != nullptr)
+		{
+			cout << Current->Data << ' ';
+			Current = Current->NextNode;
+		}
+		cout << '\n';
+	}
+
+	void AppendNode(Node* NewNode)
+	{
+		if (Head == nullptr)
+		{
+			Head = NewNode;
+			Tail = NewNode;
+		}
+		else
+		{
+			Tail->NextNode = NewNode;
+			Tail = NewNode;
+		}
+		Count++;
+	}
+
+	int Find(int TargetData)
+	{
+		Node* Current = Head;
+		int Result = 0;
+		while (Current != nullptr)
+		{
+			if (Current->Data == TargetData) Result++;
+
+			Current = Current->NextNode;
+		}
+
+		return Result;
+	}
+
+
+private:
+	Node* Head;
+	Node* Tail;
+	int Count;
+};
+
+int N, Input;
 
 int main()
 {
-    cin >> N;
-    
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> Input;
-        Map[Input]++;
-    }
-    
-    cin >> Target;
-    
-    cout << Map[Target] << '\n';
-    
-    return 0;
+	ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+
+	cin >> N;
+
+	SingleLinkedList* SLL = new SingleLinkedList();
+
+	for (int i = 1; i <= N; ++i)
+	{
+		cin >> Input;
+		SLL->AppendNode(new Node(Input));
+	}
+
+	cin >> Input;
+
+	cout << SLL->Find(Input) << '\n';
+
+	delete SLL;
+
+	return 0;
 }
