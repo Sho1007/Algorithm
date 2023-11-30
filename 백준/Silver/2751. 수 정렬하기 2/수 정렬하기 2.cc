998c7499b76c487928f4a02dab3aedba94d07937
oshyeon <31685487+Sho1007@users.noreply.github.com>
2023-11-30 09:44:58 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 384 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,26 +1,79 @@
 #include <iostream>
-#include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int N;
+vector<int> Vector;
 
-int arr[1'000'000];
+void Merge(int Start, int Mid, int End)
+{
+	int Left = Start;
+	int Right = Mid + 1;
+	int Dest = 0;
+
+	vector<int> Destination = vector<int>(End - Start + 1);
+
+	while (Left <= Mid && Right <= End)
+	{
+		if (Vector[Left] <= Vector[Right])
+		{
+			Destination[Dest++] = Vector[Left++];
+		}
+		else
+		{
+			Destination[Dest++] = Vector[Right++];
+		}
+	}
+
+	while (Left <= Mid)
+	{
+		Destination[Dest++] = Vector[Left++];
+	}
+
+	while (Right <= End)
+	{
+		Destination[Dest++] = Vector[Right++];
+	}
+
+	for (int i = 0; i < Destination.size(); ++i)
+	{
+		Vector[Start + i] = Destination[i];
+	}
+}
+
+void MergeSort(int Start, int End)
+{
+	if (Start == End)
+	{
+		return;
+	}
+
+	int Mid = (Start + End) / 2;
+
+	MergeSort(Start, Mid);
+	MergeSort(Mid + 1, End);
+
+	Merge(Start, Mid, End);
+}
 
 int main()
 {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	cin >> N;
 
+	Vector = vector<int>(N);
+
 	for (int i = 0; i < N; ++i)
 	{
-		scanf("%d", &arr[i]);
+		cin >> Vector[i];
 	}
 
-	sort(arr, arr + N);
+	MergeSort(0, Vector.size() - 1);
 
-	for (int i = 0; i < N; ++i)
+	for (int i = 0; i < Vector.size(); ++i)
 	{
-		cout << arr[i] << '\n';
+		cout << Vector[i] << '\n';
 	}
 
 	return 0;
