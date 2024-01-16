b524b0f8345ae48dc66348b97cc6f819e99ae0ec
oshyeon <31685487+Sho1007@users.noreply.github.com>
2024-01-16 20:41:41 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,27 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int N, X, Input;
+int N;
+int X;
+int Input;
 
-typedef struct Node
+struct Node
 {
     int Data;
     Node* NextNode;
     
-    Node(int NewData) : Data(NewData) {}
-} Node;
+    Node(int NewData) : Data(NewData), NextNode(nullptr) {}
+};
 
 class SingleLinkedList
 {
-public:
-    SingleLinkedList() : Head(nullptr), Tail(nullptr), Count(0) {}
+public :
+    SingleLinkedList() : Head(nullptr), Tail(nullptr) {}
     ~SingleLinkedList()
     {
         while (Head != nullptr)
         {
             Node* DeleteNode = Head;
             Head = Head->NextNode;
-            delete  DeleteNode;
+            
+            delete DeleteNode;
+        }
+    }
+    
+    void FindAndPrint(int Target)
+    {
+        Node* CurrentNode = Head;
+        while (CurrentNode != nullptr)
+        {
+            if (CurrentNode->Data < Target)
+            {
+                cout << CurrentNode->Data << ' ';
+            }
+            CurrentNode = CurrentNode->NextNode;
         }
     }
     
@@ -37,29 +53,11 @@ public:
             Tail->NextNode = NewNode;
             Tail = NewNode;
         }
-        
-        Count++;
     }
     
-    void PrintNode(int TargetData)
-    {
-        Node* Current = Head;
-        while (Current != nullptr)
-        {
-            if (Current->Data < TargetData)
-            {
-                cout << Current->Data << ' ';
-            }
-            
-            Current = Current->NextNode;
-        }
-        cout << '\n';
-    }
-    
-private:
+private :
     Node* Head;
     Node* Tail;
-    int Count;
 };
 
 int main()
@@ -72,11 +70,11 @@ int main()
     
     for (int i = 0; i < N; ++i)
     {
-        cin>> Input;
+        cin >> Input;
         SLL->AppendNode(new Node(Input));
     }
     
-    SLL->PrintNode(X);
+    SLL->FindAndPrint(X);
     
     delete SLL;
     
