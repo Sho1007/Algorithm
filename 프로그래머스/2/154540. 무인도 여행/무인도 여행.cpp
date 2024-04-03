e17afff8a0442474cfaca3d99dcdb887e0e32423
oshyeon <31685487+Sho1007@users.noreply.github.com>
2024-04-03 18:24:41 +0900

    [level 2] Title: 무인도 여행, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
@@ -1,65 +1,67 @@
 #include <string>
-#include <vector>
 #include <queue>
 #include <algorithm>
+
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+vector<vector<bool>> Visit;
+int dy[4] = {0, 0, -1, 1};
+int dx[4] = {-1, 1, 0, 0};
 
-vector<vector<bool>> VisitVector;
-vector<int> Answer;
-void Visit(vector<string>& Maps, int Y, int X)
+int BFS(int Y, int X, const vector<string>& maps)
 {
-    //cout << "Go To " << Y << ", " << X << '\n';
-    queue<pair<int,int>> PosQueue;
+    queue<pair<int,int>> Queue;
     
-    VisitVector[Y][X] = true;
-    PosQueue.push({Y, X});
+    Visit[Y][X] = true;
+    int Day = maps[Y][X] - '0';
+    Queue.push({Y,X});
     
-    int Result = 0;
-    
-    while (!PosQueue.empty())
+    while(Queue.empty() == false)
     {
-        pair<int,int> CurrentPos = PosQueue.front(); PosQueue.pop();
-        
-        Result += (Maps[CurrentPos.first][CurrentPos.second]) - '0';
-        //cout << CurrentPos.first << ", " << CurrentPos.second << " : " << Result << '\n';
+        pair<int,int> Current = Queue.front(); Queue.pop();
         
         for (int i = 0; i < 4; ++i)
         {
-            int NextY = CurrentPos.first + dy[i];
-            int NextX = CurrentPos.second + dx[i];
+            int NextY = Current.first + dy[i];
+            int NextX = Current.second + dx[i];
             
-            if (NextY < 0 || NextY >= Maps.size() || NextX < 0 || NextX >= Maps[0].size()) continue;
-            if (Maps[NextY][NextX] == 'X' || VisitVector[NextY][NextX] == true) continue;
-            VisitVector[NextY][NextX] = true;
-            PosQueue.push({NextY, NextX});
+            if (NextY < 0 || NextY >= maps.size() || NextX < 0 || NextX >= maps[0].length()) continue;
+            if (maps[NextY][NextX] == 'X' || Visit[NextY][NextX] == true) continue;
+            
+            Visit[NextY][NextX] = true;
+            Day += maps[NextY][NextX] - '0';
+            Queue.push({NextY,NextX});
         }
     }
     
-    Answer.push_back(Result);
+    return Day;
 }
 
 vector<int> solution(vector<string> maps) {
+    vector<int> answer;
     
-    
-    VisitVector = vector<vector<bool>>(maps.size(), vector<bool>(maps[0].size(), false));
+    Visit = vector<vector<bool>>(maps.size(), vector<bool>(maps[0].length(), false));
     
     for (int i = 0; i < maps.size(); ++i)
     {
-        for (int j = 0; j < maps[i].size(); ++j)
+        for (int j = 0; j < maps[i].length(); ++j)
         {
-            if (maps[i][j] != 'X' && VisitVector[i][j] == false)
+            if (maps[i][j] != 'X' && Visit[i][j] == false)
             {
-                Visit(maps, i, j);
+                answer.push_back(BFS(i, j, maps));
             }
         }
     }
     
-    sort(Answer.begin(), Answer.end());
     
-    if (Answer.size() == 0) Answer.push_back(-1);
+    if (answer.size() == 0)
+    {
+        answer.push_back(-1);
+    }
+    else
+    {
+        sort(answer.begin(), answer.end());
+    }
     
-    return Answer;
+    return answer;
 }
