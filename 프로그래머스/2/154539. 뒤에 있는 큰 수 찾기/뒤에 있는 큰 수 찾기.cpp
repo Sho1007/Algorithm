580678f91337bbd7182d2763a75e2b3ac40384b1
oshyeon <31685487+Sho1007@users.noreply.github.com>
2024-07-22 21:50:18 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 412.61 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -4,27 +4,36 @@
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    vector<int> answer(numbers.size());
+    vector<int> answer = vector<int>(numbers.size(), -1);    
     
-    for (int i = numbers.size() -1; i >= 0; --i)
+    for (int i = numbers.size()-2; i >= 0; --i)
     {
-        int Pos = i + 1;
-        while (Pos != -1)
+        if (numbers[i] < numbers[i+1])
         {
-            if (Pos == numbers.size()) Pos = -1;
-            else
+            answer[i] = i+1;
+        }
+        else if (answer[i+1] != -1)
+        {
+            int current = answer[i+1];
+            
+            while (current != -1)
             {
-                if (numbers[i] < numbers[Pos]) break;
-                else Pos = answer[Pos];
+                if (numbers[i] < numbers[current])
+                {
+                    answer[i] = current;
+                    break;
+                }
+                else
+                {
+                    current = answer[current];
+                }
             }
         }
-        answer[i] = Pos;
     }
     
-    for (int i = 0; i < answer.size(); ++i)
+    for (int i = 0; i < numbers.size(); ++i)
     {
         if (answer[i] == -1) continue;
-        
         answer[i] = numbers[answer[i]];
     }
     
