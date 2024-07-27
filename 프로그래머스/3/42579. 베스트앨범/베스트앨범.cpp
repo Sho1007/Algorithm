80e949e6ce76830e966bb4994d433230ad0a7b8c
oshyeon <31685487+Sho1007@users.noreply.github.com>
2024-07-27 09:15:53 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,33 +1,29 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <unordered_map>
 #include <iostream>
+#include <algorithm>
+
 
 using namespace std;
 
-map<string, int> genresPlayCount;
-map<string, vector<pair<int,int>>> indexMap;
-
+unordered_map<string,int> genrePlayCountMap;
+vector<pair<string,int>> genrePlayCountVector;
+unordered_map<string, vector<pair<int,int>>> genreMap;
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     
-    for (int i = 0; i < plays.size(); ++i)
+    for (int i = 0; i < genres.size(); ++i)
     {
-        genresPlayCount[genres[i]] += plays[i];
-        indexMap[genres[i]].push_back({i, plays[i]});
+        genrePlayCountMap[genres[i]] += plays[i];
+        genreMap[genres[i]].push_back({i, plays[i]});
     }
     
-    vector<pair<string,int>> genresPlayCountVector(genresPlayCount.begin(), genresPlayCount.end());
-    sort(genresPlayCountVector.begin(), genresPlayCountVector.end(), [](pair<string, int>& A, pair<string, int>& B)->bool{
-        return A.second > B.second;
-    });
-    
-    for (int i = 0; i < genresPlayCountVector.size(); ++i)
+    for (auto& genreIter : genreMap)
     {
-        vector<pair<int, int>>& indexVector = indexMap[genresPlayCountVector[i].first];
-        sort(indexVector.begin(), indexVector.end(), [](pair<int, int>& A, pair<int, int>& B)->bool
+        vector<pair<int,int>>& genreVector = genreIter.second;
+        sort(genreVector.begin(), genreVector.end(), [](pair<int,int>& A, pair<int,int>& B)->bool
              {
                  if (A.second == B.second)
                  {
@@ -36,11 +32,20 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
                  
                  return A.second > B.second;
              });
-        
-        answer.push_back(indexVector[0].first);
-        if (indexVector.size() > 1)
+    }
+    
+    genrePlayCountVector = vector<pair<string,int>>(genrePlayCountMap.begin(), genrePlayCountMap.end());
+    sort(genrePlayCountVector.begin(), genrePlayCountVector.end(), [](pair<string,int>& A, pair<string,int>& B)->bool
+         {
+             return A.second > B.second;
+         });
+    
+    for (const auto& genre : genrePlayCountVector)
+    {
+        answer.push_back(genreMap[genre.first][0].first);
+        if (genreMap[genre.first].size() > 1)
         {
-            answer.push_back(indexVector[1].first);
+            answer.push_back(genreMap[genre.first][1].first);
         }
     }
     
