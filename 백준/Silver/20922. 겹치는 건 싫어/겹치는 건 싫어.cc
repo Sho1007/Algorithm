8983e660ebd4e5dcdc9d7e08dfd236c21616b75b
oshyeon <31685487+Sho1007@users.noreply.github.com>
2025-03-15 15:42:23 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 96 ms, Memory: 7556 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -1,37 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <map>
 using namespace std;
 
 int N, K, StartPos = 0, CurrentLength = 0, MaxLength = 0;
-map<int, queue<int>> PosQueueMap;
-vector<int> Array;
+int Array[200'001] = {};
+map<int, int> CountMap;
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     cin >> N >> K;
-    Array = vector<int>(N);
 
     for (int i = 0; i < N; ++i)
     {
         cin >> Array[i];
-        PosQueueMap[Array[i]].push(i);
-        if (PosQueueMap[Array[i]].size() > K)
+        if (++CountMap[Array[i]] > K)
         {
-            // 길이 절삭
-            int RemovePos = PosQueueMap[Array[i]].front();
-            PosQueueMap[Array[i]].pop();
-
-            // 시작 지점부터 RemovePos까지의 등장 숫자들을 제거
-            for (int j = StartPos; j < RemovePos; ++j)
+            for (; StartPos < i; ++StartPos)
             {
-                PosQueueMap[Array[j]].pop();
+                CountMap[Array[StartPos]]--;
+                if (Array[StartPos] == Array[i])
+                {
+                    StartPos++;
+                    break;
+                }
             }
-            CurrentLength = i - RemovePos;
-            StartPos = RemovePos + 1;
+
+            CurrentLength = i - StartPos + 1;
         }
         else
         {
