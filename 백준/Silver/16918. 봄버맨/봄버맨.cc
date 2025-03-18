3b2628ee182ae10729a1058bbf47e31d729e9c92
oshyeon <31685487+Sho1007@users.noreply.github.com>
2025-03-18 15:36:08 +0900

    [Silver I] Title: 봄버맨, Time: 0 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.cc
+++ 백준/Silver/16918. 봄버맨/봄버맨.cc
@@ -2,76 +2,53 @@
 #include <vector>
 using namespace std;
 
-int R, C, N, Time;
-vector<vector<char>> Board;
-vector<pair<int, int>> CurrentBombPos;
-vector<vector<pair<int, int>>> BombPos;
+int R, C, N;
+char Input;
+vector<vector<int>> Board;
+vector<pair<int, int>> BombPos;
 int dY[4] = { 0, 0, -1, 1 };
 int dX[4] = { -1, 1, 0, 0 };
 
 void PlantBomb()
 {
-    CurrentBombPos.clear();
     for (int i = 0; i < R; ++i)
     {
         for (int j = 0; j < C; ++j)
         {
-            if (Board[i][j] == '.')
+            if (Board[i][j] == 0)
             {
-                Board[i][j] = 'O';
-                CurrentBombPos.push_back({ i, j });
+                Board[i][j] = 3;
+            }
+        }
+    }
+}
+
+void SpendTime()
+{
+    BombPos.clear();
+
+    for (int i = 0; i < R; ++i)
+    {
+        for (int j = 0; j < C; ++j)
+        {
+            if (Board[i][j] == 0) continue;
+
+            if (--Board[i][j] == 0)
+            {
+                BombPos.push_back({ i, j });
             }
         }
     }
 
-    BombPos.push_back(CurrentBombPos);
-}
-
-bool CheckIsRemain(pair<int,int> CurrentPos)
-{
-    //Time - 5 의 BombPos 에 영향을 받는지 확인
-    int CheckTime = Time - 5;
-    for (int i = 0; i < BombPos[CheckTime].size(); ++i)
+    for (int i = 0; i < BombPos.size(); ++i)
     {
-        int CurrentBombY = BombPos[CheckTime][i].first;
-        int CurrentBombX = BombPos[CheckTime][i].second;
-
-        if (CurrentPos.first == CurrentBombY && CurrentPos.second == CurrentBombX) return false;
-
         for (int j = 0; j < 4; ++j)
         {
-            int NextBombY = CurrentBombY + dY[j];
-            int NextBombX = CurrentBombX + dX[j];
-
-            if (NextBombY < 0 || NextBombY >= R || NextBombX < 0 || NextBombX >= C) continue;
-            if (CurrentPos.first == NextBombY && CurrentPos.second == NextBombX) return false;
-        }
-    }
-
-    return true;
-}
-
-void Boom()
-{
-    // 현재 시간 대비 3초 전
-    int InTime = Time - 3;
-    for (int i = 0; i < BombPos[InTime].size(); ++i)
-    {
-        if (InTime > 0 && CheckIsRemain(BombPos[InTime][i]) == false)
-        {
-            continue;
-        }
-
-        Board[BombPos[InTime][i].first][BombPos[InTime][i].second] = '.';
-
-        for (int j = 0; j < 4; ++j)
-        {
-            int NextY = BombPos[InTime][i].first + dY[j];
-            int NextX = BombPos[InTime][i].second + dX[j];
+            int NextY = BombPos[i].first + dY[j];
+            int NextX = BombPos[i].second + dX[j];
 
             if (NextY < 0 || NextY >= R || NextX < 0 || NextX >= C) continue;
-
-            Board[NextY][NextX] = '.';
+            Board[NextY][NextX] = 0;
         }
     }
 }
@@ -82,7 +59,7 @@ void PrintBoard()
     {
         for (int j = 0; j < C; ++j)
         {
-            cout << Board[i][j];
+            cout << (Board[i][j] == 0 ? '.' : 'O');
         }
         cout << '\n';
     }
@@ -94,27 +71,19 @@ int main()
 
     cin >> R >> C >> N;
 
-    Board = vector<vector<char>>(R, vector<char>(C));
+    Board = vector<vector<int>>(R, vector<int>(C, 0));
 
     for (int i = 0; i < R; ++i)
     {
         for (int j = 0; j < C; ++j)
         {
-            cin >> Board[i][j];
-            if (Board[i][j] == 'O')
-            {
-                CurrentBombPos.push_back({ i, j });
-            }
+            cin >> Input;
+            Board[i][j] = (Input == 'O' ? 3 : 0);
         }
     }
 
-    BombPos.push_back(CurrentBombPos);
-
-    // 1초
-    Time = 1;
-    BombPos.push_back(vector<pair<int, int>>());
-    // 아무것도 하지 않음
-    if (N == Time)
+    SpendTime();
+    if (N == 1)
     {
         PrintBoard();
         return 0;
@@ -122,9 +91,7 @@ int main()
 
     N %= 4;
 
-    // 2초
-    Time++;
-    // 빈 곳에 폭탄을 설치
+    SpendTime();
     PlantBomb();
     if (N == 2)
     {
@@ -132,17 +99,14 @@ int main()
         return 0;
     }
 
-    // 3초
-    Time++;
-    Boom();
+    SpendTime();
     if (N == 3)
     {
         PrintBoard();
         return 0;
     }
 
-    // 4초
-    Time++;
+    SpendTime();
     PlantBomb();
     if (N == 0)
     {
@@ -150,9 +114,7 @@ int main()
         return 0;
     }
 
-    // 5초, N==1
-    Time++;
-    Boom();
+    SpendTime();
     PrintBoard();
 
     return 0;
