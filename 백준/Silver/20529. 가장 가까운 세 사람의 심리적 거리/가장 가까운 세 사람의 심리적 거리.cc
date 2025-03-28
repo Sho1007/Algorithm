ec631c7bfa3340e759ab726e573755a0273a032d
oshyeon <31685487+Sho1007@users.noreply.github.com>
2025-03-28 15:19:23 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 8 ms, Memory: 5152 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -1,109 +1,68 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 using namespace std;
-
-int T, N, Answer;
-
-vector<string> Array;
-char MBTI[4][2] = {
-    {'E', 'I'},
-    {'N', 'S'},
-    {'F', 'T'},
-    {'P', 'J'}}
-;
-vector<string> MBTIArray = { "" };
-
-map<string, map<string, int>> DistanceMap;
-
-vector<string> operator+ (vector<string> A, string B)
+inline int GetDistance(const string& A, const string& B)
 {
-    A.push_back(B);
-    return A;
-}
-
-void BT(int StartPos, vector<string> Target)
-{
-    if (Target.size() == 3)
+    int Distance = 0;
+    for (int i = 0; i < 4; ++i)
     {
-        int CurrentDistance = 0;
-        CurrentDistance += DistanceMap[Target[0]][Target[1]];
-        CurrentDistance += DistanceMap[Target[1]][Target[2]];
-        CurrentDistance += DistanceMap[Target[0]][Target[2]];
-
-        if (CurrentDistance < Answer)
+        if (A[i] != B[i])
         {
-            Answer = CurrentDistance;
+            Distance++;
         }
-
-        return;
     }
+    
+    return Distance;
+}
+inline int MIN(int A, int B) { return A < B ? A : B; }
 
-    for (int i = StartPos; i < N; ++i)
+int T, N;
+vector<string> Array;
+
+
+
+int TestCast()
+{
+    int Answer = 987'654'321;
+    
+    cin >> N;
+    Array = vector<string>(N);
+    
+    for (int i = 0; i < N; ++i)
     {
-        BT(i + 1, Target + Array[i]);
+        cin >> Array[i];
     }
+    
+    if (N > 32)
+    {
+        return 0;
+    }
+    
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = i + 1; j < N; ++j)
+        {
+            for (int k = j + 1; k < N; ++k)
+            {
+                Answer = MIN(Answer, GetDistance(Array[i], Array[j]) + GetDistance(Array[i], Array[k]) + GetDistance(Array[j], Array[k]));
+            }
+        }
+    }
+    
+    return Answer;
 }
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    for (int i = 0; i < 4; ++i)
-    {
-        vector<string> newMBTIArray;
-        for (int j = 0; j < 2; ++j)
-        {
-            for (const string& Iter : MBTIArray)
-            {
-                newMBTIArray.push_back(Iter + MBTI[i][j]);
-            }
-        }
-
-        MBTIArray = newMBTIArray;
-    }
-
-    for(string& First : MBTIArray)
-    {
-        for (string& Second : MBTIArray)
-        {
-            int Distance = 0;
-            for (int i = 0; i < 4; ++i)
-            {
-                if (First[i] != Second[i])
-                {
-                    Distance++;
-                }
-            }
-
-            DistanceMap[First][Second] = Distance;
-        }
-    }
-
+    
     cin >> T;
-
-    while (T-- > 0)
+    
+    while (T-->0)
     {
-        cin >> N;
-        Array = vector<string>(N);
-        for (int i = 0; i < N; ++i)
-        {
-            cin >> Array[i];
-        }
-        
-        if (N <= 32)
-        {
-            Answer = 987'654'321;
-            BT(0, {});
-        }
-        else
-        {
-            Answer = 0;
-        }
-
-        cout << Answer << '\n';
+        cout << TestCast() << '\n';
     }
-
+    
     return 0;
 }
